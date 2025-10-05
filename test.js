e56d0953819309476ca0d81c86cfb5150aa15c84
sablaygaming <sablaygaming@gmail.com>
2025-10-06 00:45:28 +0800

    a

--- test.js
+++ test.js
@@ -1,30 +1,41 @@
 
 //"user strict";
 
-const inpStr = require('prompt-sync')();
+const readline = require('readline');
 
-try {
-    let num = inpStr("input number: ");
-    let vars = "im a string";
-    num = parseInt(num, 10);
+// Setup the readline interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    if (vars === num) console.log("error input aa");
-    else console.log("valid input aa num ", num, ", vars ", vars);
-
-} catch (errs) {
-    // This code is never reached for the error above.
-    console.log("This will not catch the error!, the errs: ", errs);
+function inpStr(query) {
+    return new Promise(resolve => rl.question(query, resolve));
 }
 
 
+async function main() {
 
+    data = [];
+    data.push({ Name: "", Address: "" });
+    data[0].Name = await inpStr("Name: ");
+    data[0].Address = await inpStr("Address: ");
 
+    console.log("u inputed ", data);
+    //array of object to stringCSV
+    const headers = Object.keys(data[0]).join(",");
+    const rows = data.map(obj => Object.values(obj).join(",")).join("\n");
 
+    subData = `${headers}\n${rows}`;
+    console.log("\nresult data \n", subData);
 
+    //now create stringCsv to array of object 
+    //pause
+    await inpStr("exit prog...");
+    rl.close();
+}
 
-
-
-
+main();
 
 
 
